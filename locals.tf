3411411125ab769d8495478fa846cba483aa2b37
Amanda <amanda_gouveia_@hotmail.com>
2024-03-29 17:02:04 -0300

    feat: commit new features

---
+++ locals.tf
@@ -0,0 +1,10 @@
+locals {
+    services = jsondecode(file("${path.module}/${var.services_file}"))
+    services_by_team = {
+        for service in local.services: "${service.team}/${service.name}" => service
+    }
+    service_definition_defaults = {
+       //campos opcionais 
+    }
+
+    }
