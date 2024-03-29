3a6566e5caf88195ecdc9a2642be3ee3c59edc82
Amanda <amanda_gouveia_@hotmail.com>
2024-03-29 17:07:49 -0300

    feat: add new features

--- locals.tf
+++ locals.tf
@@ -7,4 +7,4 @@ locals {
        //campos opcionais 
     }
 
-    }
+}
--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ terraform {
   }
 }
 
-
 provider "datadog" {
   api_key = var.datadog_api_key
   app_key = var.datadog_app_key
